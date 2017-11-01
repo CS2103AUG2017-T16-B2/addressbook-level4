9ba697705bf341264b3ad10d240118b067fcfd01
Lenaldnwj <lenald95@hotmail.com>
2017-11-01 20:25:24 +0800

    Refactor phone class and improve testing for it

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -12,55 +12,62 @@ public class PhoneTest {
 
     @Test
     public void isValidPhone() {
-        // invalid phone numbers
+        // invalid phone number input format
         assertFalse(Phone.isValidPhone("")); // empty string
         assertFalse(Phone.isValidPhone(" ")); // spaces only
-        assertFalse(Phone.isValidPhone("Student: 97272031 Parent: 9797979")); // parent do not have exactly 8 digit
-        assertFalse(Phone.isValidPhone("Student: 9727 Parent: 979")); // both do not have exactly 8 digit
-        assertFalse(Phone.isValidPhone("Student: 9727 Parent: 97979797")); // student do not have exactly 8 digit
-        assertFalse(Phone.isValidPhone("Student/ 97272031 Parent/ 97979797")); // / instead of :
-        assertFalse(Phone.isValidPhone("studen: 97272031 paret: 97979797")); // wrong labelling
-        assertFalse(Phone.isValidPhone("Student: 97272031   Parent: 97979797")); // consecutive whitespaces
+        assertFalse(Phone.isValidPhone("Student: 97272131 "
+                + "Parent: 9797979")); // parent do not have exactly 8 digit
+        assertFalse(Phone.isValidPhone("Student: 972745 Parent: 979")); // both do not have exactly 8 digit
+        assertFalse(Phone.isValidPhone("Student: 9727 "
+                + "Parent: 97979797")); // student do not have exactly 8 digit
+        assertFalse(Phone.isValidPhone("Student/ 91111031 Parent/ 97971241")); // / instead of :
+        assertFalse(Phone.isValidPhone("studen: 91254311 parent: 97213197")); //  misspelled student
+        assertFalse(Phone.isValidPhone("Student: 97272121   Parent: 97314197")); // consecutive whitespaces
         assertFalse(Phone.isValidPhone("!@@#$#@$#@{}")); // random symbol
         assertFalse(Phone.isValidPhone("Student: 972jb72031 Parent: 97jhb979797")); // alphanumeric numbers
         assertFalse(Phone.isValidPhone("Student: gfxgfx Parent: gfxgfxgfx")); // purely alphabets
-        assertFalse(Phone.isValidPhone("Student: 97272031")); // must have at least parent number
-        assertFalse(Phone.isValidPhone("Parent: 97979797 "
-                + "Student: 97272031")); // parent number should be entered after student number if both have number.
-        assertFalse(Phone.isValidPhone("Parent: 972701")); // have at least parent number, but not exactly 8 digits.
+        assertFalse(Phone.isValidPhone("Student: 97270000")); // must have at least parent number
+        assertFalse(Phone.isValidPhone("Parent: 97971231 "
+                + "Student: 97272271")); // parent number should be entered after student number
+        // if both parent and student number are entered.
+        assertFalse(Phone.isValidPhone(
+                "Parent: 972701")); // have at least parent number, but not exactly 8 digits.
 
 
-        // valid phone numbers
-        assertTrue(Phone.isValidPhone("Student: 97272031 "
-                + "Parent: 97979797")); // both parent and student phone number of exactly 8 numbers
-        assertTrue(Phone.isValidPhone("Student: 87272111 "
-                + "Parent: 87767988")); // another set of parent and student phone number of exactly 8 digit numbers
-        assertTrue(Phone.isValidPhone("Parent: 87767988")); // parent number only of exactly 8 digit numbers
+        // valid phone number input format
+        assertTrue(Phone.isValidPhone("student/97272031 "
+                + "parent/97979797")); // both parent and student phone number of exactly 8 numbers
+        assertTrue(Phone.isValidPhone("student/87272111 "
+                + "parent/87767988")); // another set of parent and student phone number of exactly 8 digit numbers
+        assertTrue(Phone.isValidPhone("parent/87767988")); // parent number only of exactly 8 digit numbers
     }
 
     @Test
-    public void changeToAppropriateUiFormat() {
+    public void changeInputToUiFormat() {
         // Invalid Ui Format
-        assertNotEquals(Phone.changeToAppropriateUiFormat("student/97272030 parent/97979797"),
-                "Student:97272030 Parent:97979797"); // No spacing after Student: and Parent:
-        assertNotEquals(Phone.changeToAppropriateUiFormat("student/97272030 parent/97979797"),
-                "student:97272030 parent:97979797"); // No capital letter S and P
-        assertNotEquals(Phone.changeToAppropriateUiFormat("student/97272030 parent/97979797"),
+        assertNotEquals(Phone.changeInputToUiFormat("student/97272030 parent/97971231"),
+                "Student:97272030 Parent:97971231"); // No spacing after Student: and Parent:
+        assertNotEquals(Phone.changeInputToUiFormat("student/97272011 parent/91173321"),
+                "student:97272011 parent:91173321"); // No capital letter S and P
+        assertNotEquals(Phone.changeInputToUiFormat("student/97272030 parent/97971122"),
                 "student:97272011 parent:97979700"); // Incorrect numbers
-        assertNotEquals(Phone.changeToAppropriateUiFormat("student/97272030 parent/97979797"),
-                "student/ 97272011 parent/ 97979700"); // / does not change to :
-        assertNotEquals(Phone.changeToAppropriateUiFormat("student/97272030 parent/97979797"),
-                "student: 97272011 Parent: 97979700"); // Letter s not capitalised
-        assertNotEquals(Phone.changeToAppropriateUiFormat("student/97272030 parent/97979797"),
-                "Student: 97272011 parent: 97979700"); // Letter p not capitalised
+        assertNotEquals(Phone.changeInputToUiFormat("student:97272030 parent:97979797"),
+                "student/ 97272011 parent/ 97979700"); // Returned : instead of /
+        assertNotEquals(Phone.changeInputToUiFormat("student/97272030 parent/97979797"),
+                "student: 97272011 Parent: 97979700"); // Letter s not capitalised (Incorrect labelling)
+        assertNotEquals(Phone.changeInputToUiFormat("student/97272030 parent/97979797"),
+                "Student: 97272011 parent: 97979700"); // Letter p not capitalised (Incorrect labelling)
+        assertNotEquals(Phone.changeInputToUiFormat("student/97272030 parent/97979797"),
+                ""); // Returned empty string
 
 
-        // Valid Ui Format
-        assertEquals(Phone.changeToAppropriateUiFormat("student/97272030 parent/97979797"),
-                "Student: 97272030 Parent: 97979797"); // valid Ui format
-        assertEquals(Phone.changeToAppropriateUiFormat("student/97278977 parent/97979776"),
-                "Student: 97278977 Parent: 97979776"); // valid Ui format with another set of numbers
-        assertEquals(Phone.changeToAppropriateUiFormat("student/82278977 parent/97979776"),
+
+        // Valid Ui format
+        assertEquals(Phone.changeInputToUiFormat("student/97272030 parent/97979797"),
+                "Student: 97272030 Parent: 97979797"); // User followed correct input format
+        assertEquals(Phone.changeInputToUiFormat("parent/97979776"),
+                "Parent: 97979776"); // Student number left out (student number is a optional field)
+        assertEquals(Phone.changeInputToUiFormat("student/82278977 parent/97979776"),
                 "Student: 82278977 Parent: 97979776"); // valid Ui format with another set of numbers
     }
 }
